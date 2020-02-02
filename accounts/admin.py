a5b4c2151e797eeb5b577c5ecc57d59142e925a9
Basanta Baruwal <baruwalbasanta@gmail.com>
2020-02-02 07:25:18 +0545

    Models created and linked to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Customer, Product, Order
 
 # Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'address', 'date_created')
+    list_display_links = ('name', 'email')
+    list_filter = ('name', 'email', 'address', 'date_created')
+    list_per_page = 20
+    search_fields = ('name','address', 'date_created')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'category',)
+    list_display_links= ('name',)
+    list_filter = ('name', 'price', 'category',)
+    list_per_page = 20
+    search_fields = ('name' 'price', 'category', 'description',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('status', 'date_created')
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    address = models.CharField(max_length=100, null=True)
+    email = models.CharField(max_length=100, null=True)
+    phone = models.CharField(max_length=20, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        ordering = ('-date_created',)
+
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door'),
+    )
+    name = models.CharField(max_length=100, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=30, null=True, choices=CATEGORY)
+    description = models.TextField(null=True)
+
+    
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for Delivery', 'Out for Delivery'),
+        ('Delivered', 'Delivered'),
+        ('Canceled', 'Canceled'),
+    )
+    # customer = 
+    # product = 
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=100, null=True, choices=STATUS)
